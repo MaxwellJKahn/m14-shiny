c1556ca4b13546f96fca71654a40560d767fd311
MaxwellJKahn <maxkahn@uw.edu>
2016-11-10 15:03:44 -0800

    Worked on Exercise 2

--- exercise-2/server.R
+++ exercise-2/server.R
@@ -1,5 +1,4 @@
 ### Exercise 2 ###
-
 library(shiny)
 
 # We'll look into these more next week: http://shiny.rstudio.com/gallery/widget-gallery.html 
@@ -9,6 +8,11 @@ library(shiny)
 # It should takes as an input the number of observations, and a color
 # It should return a rendered plot
 shinyServer(function(input, output) {
-  # Save a 'scatter' property which is a renderPlot object (that renders a scatterplot)
-  
+  # You can access the value of the widget with input$select, e.g.
+  output$scatter <- renderPlot({ 
+    x <- rnorm(input$num)
+    y <- rnorm(input$num)
+    return(plot(x,y, col=input$color))
+  })
 })
+
